56def4e4fdb2970ec29983857c38d78e11c429b2
Lee Aspitarte <aspitarte@gmail.com>
2020-11-21 16:16:37 -0800

    add energy storage nlp plot

---
+++ energy_storage_nlp/generate_df_info.py
@@ -0,0 +1,34 @@
+#%% 
+import pandas as pd
+import sqlite3
+import os
+import sys
+
+data_folder = r'C:\Users\aspit\Git\MHDLab-Projects\Energy Storage\data'
+
+con = sqlite3.connect(os.path.join(data_folder, 'nlp.db'))
+cursor = con.cursor()
+
+df = pd.read_sql_query("SELECT * FROM texts", con, index_col='ID')
+
+df = df.dropna(subset=['processed_text'])
+df = df[df['language'] == 'en']
+
+#%%
+
+# %%
+num_papers = str(len(df))
+
+print('Number of papers: ' + num_papers)
+
+# %%
+search_terms = ", ".join(set(df['searchterm']))
+
+
+print('Search Terms: ' + search_terms)
+
+
+
+
+
+# %%
